8c7bff8938ea4aa595c8fd7537beb76db53fc49f
Gopal kumar <91552766+gopalkumr@users.noreply.github.com>
2023-04-02 07:48:51 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/testapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.neop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/Home/homescreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:neop/main.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello User, Welcome'),
+      ),
+    );
+  }
+}
