0505e55606de0f3b1eb56976a252587a51bde9f8
Gopal kumar <91552766+gopalkumr@users.noreply.github.com>
2023-04-16 13:18:10 +0530

    key hidden

--- lib/src/providers/appwrite_client.dart
+++ lib/src/providers/appwrite_client.dart
@@ -1,21 +1,11 @@
 import 'package:appwrite/appwrite.dart';
-
-/*
+import 'appwrite_key.dart';
 
 class AppwriteClient {
   static Client getClient() {
     return Client()
-      ..setEndpoint('http://35.231.3.242/v1') // Your API Endpoint
-      ..setProject('64305dfd6509db5faf1f')
-      ..setSelfSigned(status: true); // Your project ID;
-  }
-}
-*/
-class AppwriteClient {
-  static Client getClient() {
-    return Client()
-      ..setEndpoint('http://35.224.169.73/v1') // Your API Endpoint
-      ..setProject('643185a448ddb4226d70')
+      ..setEndpoint(endpoint) // Your API Endpoint
+      ..setProject(project_id)
       ..setSelfSigned(status: true); // Your project ID;
   }
 }
