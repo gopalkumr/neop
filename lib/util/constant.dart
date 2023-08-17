5bad0b6baa659c0819fea84908575000e019ed1d
Gopal kumar <91552766+gopalkumr@users.noreply.github.com>
2023-08-17 15:19:12 +0530

    the api of appwrite has been updated.

--- lib/util/constant.dart
+++ lib/util/constant.dart
@@ -9,51 +9,50 @@ const COLOR_BACKGROUND_LIGHT = Color(0xFFF1F3F6);
 
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
-  backgroundColor: COLOR_BACKGROUND,
   primaryColor: COLOR_PRIMARY,
   cardColor: const Color(0xffF1F3F6),
   iconTheme: const IconThemeData(
     color: Color(0xFF3A4276),
   ),
   textTheme: TextTheme(
-    button: GoogleFonts.poppins(
+    labelLarge: GoogleFonts.poppins(
       fontSize: 15,
       height: 1.6,
       color: const Color(0xff212330),
       fontWeight: FontWeight.w600,
     ),
-    bodyText2: GoogleFonts.poppins(
+    bodyMedium: GoogleFonts.poppins(
       fontSize: 12,
       color: const Color(0xff1B1D28),
       fontWeight: FontWeight.w500,
     ),
-    bodyText1: GoogleFonts.poppins(
+    bodyLarge: GoogleFonts.poppins(
       height: 1.6,
       fontSize: 12,
       color: const Color(0xff7b7f9e),
       fontWeight: FontWeight.w400,
     ),
-    headline2: GoogleFonts.poppins(
+    displayMedium: GoogleFonts.poppins(
       fontSize: 24,
       color: const Color(0xff171822),
       fontWeight: FontWeight.w600,
     ),
-    headline3: GoogleFonts.poppins(
+    displaySmall: GoogleFonts.poppins(
       fontSize: 22,
       color: const Color(0xff3A4276),
       fontWeight: FontWeight.w800,
     ),
-    headline4: GoogleFonts.poppins(
+    headlineMedium: GoogleFonts.poppins(
       fontSize: 15,
       color: const Color(0xff3A4276),
       fontWeight: FontWeight.w600,
     ),
-    headline5: GoogleFonts.poppins(
+    headlineSmall: GoogleFonts.poppins(
       fontSize: 22,
       color: Colors.black,
       fontWeight: FontWeight.w600,
     ),
-    headline6: GoogleFonts.poppins(
+    titleLarge: GoogleFonts.poppins(
       fontSize: 20,
       color: Colors.black,
       fontWeight: FontWeight.w500,
@@ -63,7 +62,10 @@ ThemeData lightTheme = ThemeData(
       color: Colors.black,
       fontWeight: FontWeight.w700,
     ),
-  ), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: COLOR_BACKGROUND_LIGHT),
+  ),
+  colorScheme: ColorScheme.fromSwatch()
+      .copyWith(secondary: COLOR_BACKGROUND_LIGHT)
+      .copyWith(background: COLOR_BACKGROUND),
 );
 
 ThemeData darkTheme = ThemeData(
